0cb12978670a669c2ba342135c5998ae81d02cce
lesleyjwu <98455428+lesleyjwu@users.noreply.github.com>
2023-04-17 21:12:28 -0400

    EDA is for EDA and DSC code is the ML part

---
+++ DSCcode.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
+# In[3]:
+
+
+import os
+df = pd.read_csv("cleveland.data")
+
+
+# In[ ]:
+
+
+
+
